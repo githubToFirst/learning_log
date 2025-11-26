36ef50b7e6106c017ae26bc0375563555595dfc6
gphc <13296601413m0@sina.cn>
2025-11-26 14:28:25 +0800

    提交测试

---
+++ learning_logs/forms.py
@@ -0,0 +1,19 @@
+# python 3.13
+# 创建第一个表单
+
+from  django import forms
+from .models import Topic
+from .models import Entry
+
+class TopicForm(forms.ModelForm):
+    class Meta:
+        model = Topic
+        fields = ['text']
+        labels = {'text':''}
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text':''}
+        widgets = {'text':forms.Textarea(attrs={'cols':80})}
---
+++ learning_logs/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Topic(models.Model):
+    """用户学习的主题 """
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    #当用户被删除时，所有与之关联的主题也会被删除
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        """返回模型的字符串表示"""
+        return self.text
+
+
+class Entry(models.Model):
+    """学到的有关主题的具体知识"""
+    topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural= 'entries'
+
+    def __str__(self):
+        """返回一个表示条目的简单字符串"""
+        return f"{self.text[:50]}..."
